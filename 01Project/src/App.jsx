2cdac72e0cbee7f9783f0c553219192996d6f4cf
aayush_09sh10-droid <aayush09sh10@gmail.com>
2026-01-10 12:50:50 +0530

    Project

---
+++ 01Project/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Aayush from "./aayush.jsx"
+
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+    <Aayush/>
+    <h1>React {username}</h1>
+    
+   </>
+  )
+}
+
+export default App
---
+++ 01Project/src/aayush.jsx
@@ -0,0 +1,7 @@
+function Aayush(){
+    return(
+        <h3>Aayush!</h3>
+    )
+}
+
+export default Aayush
